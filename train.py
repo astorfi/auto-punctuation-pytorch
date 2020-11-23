33636001ed136d1d1c3af182e8ec10eb08cd15ac
Amirsina Torfi <amirsina.torfi@gmail.com>
2020-11-23 17:27:23 -0500

    tmp

--- train.py
+++ train.py
@@ -4,65 +4,68 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
 import torch
-
-# Some parameter
-BATCH_TO_SHOW_ACCURACY = 25
-BATCH_TO_SHOW_PREDICTION = 100
-
 import utils, data, metric, model
 from tqdm import tqdm
 import numpy as np
-from IPython.display import HTML, clear_output
+
+""" # Some parameter
+"""
+BATCH_TO_SHOW_ACCURACY = 25
+BATCH_TO_SHOW_PREDICTION = 100
+batch_size = 64
+
+CHARS = "\x00 ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890.,;:?\"'\n\r\t~!@#$%^&*()-/–—=_+<>{}[]|\\`~\xa0ëµ£"
+CHAR_DICT = {ch: i for i, ch in enumerate(CHARS)}
+
 
 input_chars = list(" \nabcdefghijklmnopqrstuvwxyz01234567890")
 output_chars = ["<nop>", "<cap>"] + list(".,?!")
 
-# torch.set_num_threads(8)
-batch_size = 64
-
 char2vec = utils.Char2Vec(chars=input_chars, add_unknown=True)
 output_char2vec = utils.Char2Vec(chars=output_chars)
-input_size = char2vec.size
-output_size = output_char2vec.size
 
-print("input_size is: " + str(input_size) + "; ouput_size is: " + str(output_size))
-hidden_size = input_size
-layers = 1
 
-# Implement mini-batch
 class Model(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, batch_size=1, layers=1, bi=False):
-        """
-        IMPORTANT: Use batch_first convention for ease of use.
-                   However, the hidden layer still use batch middle convension.
-        """
+    def __init__(self, input_size, hidden_size, output_size, batch_size=1, num_layers=1, bi=False):
         super(Model, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
         self.batch_size = batch_size
-        self.layers = layers
+        self.num_layers = num_layers
         self.bi_mul = 2 if bi else 1
 
-        self.encoder = nn.Linear(input_size, hidden_size)
-        self.gru = nn.GRU(input_size, hidden_size, self.layers, bidirectional=bi, batch_first=True)
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, self.num_layers, bidirectional=bi, batch_first=True)
         self.decoder = nn.Linear(hidden_size * self.bi_mul, output_size)
-        self.softmax = F.softmax
 
     def forward(self, x, hidden):
         embeded = x
-        gru_output, hidden = self.gru(embeded, hidden.view(self.layers * self.bi_mul, -1, self.hidden_size))
+        gru_output, hidden = self.gru(embeded, hidden.view(self.num_layers * self.bi_mul, self.batch_size, self.hidden_size))
         output = self.decoder(gru_output.contiguous().view(-1, self.hidden_size * self.bi_mul))
         return output.view(self.batch_size, -1, self.output_size), hidden
 
     def init_hidden(self):
-        return Variable(torch.zeros(self.layers * self.bi_mul, self.batch_size, self.hidden_size))
+        return Variable(torch.zeros(self.num_layers * self.bi_mul, self.batch_size, self.hidden_size))
 
-model = Model(input_size, hidden_size, output_size, batch_size=batch_size, layers=layers, bi=True)
+""" # Model
+Here we initialize the model with its associated parameters.
+"""
+# How many GRU layers to be stacked
+num_layers = 1
+input_size = char2vec.size
+output_size = output_char2vec.size
+hidden_size = 32
 
-learning_rate = 0.5e-2
+# Model initialization
+model = Model(input_size, hidden_size, output_size, batch_size=batch_size, num_layers=num_layers, bi=True)
+
+""" # Optimizer & Loss
+"""
+learning_rate = 0.001
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-optimizer.zero_grad()
+
+# Refer to https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html
 loss_fn = nn.CrossEntropyLoss()
 
 
@@ -94,8 +97,13 @@ for epoch_num in range(24):
             # Reset gradients
             optimizer.zero_grad()
 
-            # Get the embedding layer
-            embeded = Variable(char2vec.one_hot_batch(input_))
+            # Characters to indexes
+            source_shape = [len(input_), len(input_[0])]
+            char_to_idx = torch.LongTensor([[[char2vec.get_ind(char)] for char in src] for src in input_])
+            char_to_idx = char_to_idx[..., 0]
+
+            # Get the embedding
+            embeded = model.embedding(torch.LongTensor(char_to_idx))
 
             # Forward pass to the model
             output, hidden = model(embeded, hidden)
@@ -119,9 +127,9 @@ for epoch_num in range(24):
             print('Epoch {:d} Batch {}'.format(epoch_num + 1, batch_ind + 1))
             print("=================================")
             temperature = 1
-            softmax = model.softmax(output.view(-1, model.output_size) / temperature, 1
+            out = F.softmax(output.view(-1, model.output_size) / temperature, 1
                                               ).view(model.batch_size, -1, model.output_size)
-            indexes = torch.multinomial(softmax.view(-1, model.output_size), 1
+            indexes = torch.multinomial(out.view(-1, model.output_size), 1
                                         ).view(model.batch_size, -1)
             punctuation_output = output_char2vec.vec2list_batch(indexes)
 
