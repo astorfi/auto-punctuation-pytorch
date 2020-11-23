b4949bfaaea0cfa1e16a61cf5e65d1dbde389b46
Amirsina Torfi <amirsina.torfi@gmail.com>
2020-11-23 12:10:42 -0500

    check

--- train.py
+++ train.py
@@ -5,23 +5,6 @@ import torch.optim as optim
 from torch.autograd import Variable
 import torch
 
-# vis = visdom.Visdom()
-# vis.env = "deep-punc-experiment"
-
-emb = None
-opt = None
-smx = None
-lss = None
-
-
-def plot_progress(embeded, output, softmax, losses):
-    global emb, opt, smx, lss
-    emb = vis.heatmap(embeded, win=emb or None, opts=dict(title="Input Embedding"))
-    opt = vis.heatmap(output, win=opt or None, opts=dict(title="GRU Output"))
-    smx = vis.heatmap(softmax, win=smx or None, opts=dict(title="Softmax Activation"))
-    lss = vis.line(Y=losses, win=lss or None, opts=dict(title="Loss"))
-
-
 input_chars = list(" \nabcdefghijklmnopqrstuvwxyz01234567890")
 output_chars = ["<nop>", "<cap>"] + list(".,;:?!\"'$")
 
@@ -137,10 +120,6 @@ for epoch_num in range(24):
             indexes = torch.multinomial(softmax.view(-1, rnn_model.output_size), 1
                                         ).view(rnn_model.batch_size, -1)
             punctuation_output = egdt.output_char2vec.vec2list_batch(indexes)
-            # plot_progress(egdt.embeded[0, :400].data.numpy().T,
-            #               egdt.output[0, :400].data.numpy().T,
-            #               egdt.softmax[0, :400].data.numpy().T,
-            #               np.array(egdt.losses))
 
             metric.print_pc(utils.flatten(punctuation_output), utils.flatten(target_))
             print('\n')
@@ -151,56 +130,3 @@ for epoch_num in range(24):
                                      punctuation_output[0])
             print(validate_target)
             print(result)
-
-
-sys.exit()
-learning_rate = 0.5e-2
-egdt.setup_training(learning_rate)
-
-seq_length = 500
-
-for epoch_num in range(24):
-
-    for batch_ind, (max_len, sources) in enumerate(tqdm(data.batch_gen(data.train_gen(), batch_size))):
-
-        # prepare the input and output chunks
-        input_srcs = []
-        punc_targs = []
-        for chunk in sources:
-            input_source, punctuation_target = data.extract_punc(chunk, egdt.char2vec.chars, egdt.output_char2vec.chars)
-            input_srcs.append(input_source)
-            punc_targs.append(punctuation_target)
-
-            # at the begining of the file, reset hidden to zero
-        egdt.init_hidden_(random=False)
-        seq_len = data.fuzzy_chunk_len(max_len, seq_length)
-        for input_, target_ in zip(zip(*[data.chunk_gen(seq_len, src) for src in input_srcs]),
-                                   zip(*[data.chunk_gen(seq_len, tar, ["<nop>"]) for tar in punc_targs])):
-
-            try:
-                egdt.forward(input_, target_)
-                egdt.descent()
-            except KeyError:
-                raise KeyError
-
-        if batch_ind % 25 == 24:
-            print('Epoch {:d} Batch {}'.format(epoch_num + 1, batch_ind + 1))
-            print("=================================")
-            punctuation_output = egdt.output_chars()
-            # plot_progress(egdt.embeded[0, :400].data.numpy().T,
-            #               egdt.output[0, :400].data.numpy().T,
-            #               egdt.softmax[0, :400].data.numpy().T,
-            #               np.array(egdt.losses))
-
-            metric.print_pc(utils.flatten(punctuation_output), utils.flatten(target_))
-            print('\n')
-
-        if batch_ind % 100 == 99:
-            validate_target = data.apply_punc(input_[0], target_[0])
-            result = data.apply_punc(input_[0],
-                                     punctuation_output[0])
-            print(validate_target)
-            print(result)
-
-    # print('Dev Set Performance {:d}'.format(epoch_num))
-    egdt.save('./data/engadget_train_epoch-{}_batch-{}.tar'.format(epoch_num + 1, batch_ind + 1))
