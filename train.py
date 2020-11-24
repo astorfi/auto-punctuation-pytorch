e0e161a10d43b6015287148dc7b9147e6b7fcf6f
Amirsina Torfi <amirsina.torfi@gmail.com>
2020-11-24 16:55:26 -0500

    reformat code

--- train.py
+++ train.py
@@ -46,13 +46,13 @@ df_path = os.path.expanduser("data/data.h5")
 
 # Load hdf5 data
 data_df = pd.read_hdf(df_path, 'df')
-dataset = list(data_df.text)
+loaded_dataset = list(data_df.text)
 
 
 class DatasetObject(Dataset):
     """Face Landmarks dataset."""
 
-    def __init__(self, dataset, trainData):
+    def __init__(self, dataset):
         """
         Args:
             csv_file (string): Path to the csv file with annotations.
@@ -60,12 +60,7 @@ class DatasetObject(Dataset):
             transform (callable, optional): Optional transform to be applied
                 on a sample.
         """
-
-        data_len = len(dataset)
-        if trainData:
-            self.data = dataset[:int(0.8 * data_len)]
-        else:
-            self.data = dataset[int(0.8 * data_len):]
+        self.data = dataset
 
     def __len__(self):
         return len(self.data)
@@ -82,20 +77,29 @@ class DatasetObject(Dataset):
         return sent_len, sent
 
 
-""" # Dataset creation
-"""
-trainData = DatasetObject(dataset=dataset, trainData=True)
-train_loader = torch.utils.data.DataLoader(trainData,
-                                           batch_size=config['batch_size'], shuffle=True,
-                                           num_workers=0, pin_memory=True, drop_last=True)
+def get_data(dataset, train=True):
 
-testData = DatasetObject(dataset=dataset, trainData=False)
-test_loader = torch.utils.data.DataLoader(testData,
-                                          batch_size=config['batch_size'], shuffle=True,
-                                          num_workers=1, drop_last=True)
+    # Calculate len dataset
+    data_len = len(dataset)
 
-# Sample from data loaders
-sent_sample, sent_len_sample = next(iter(test_loader))
+    # First split to train/test
+    if train:
+        sub_dataset = dataset[:int(0.8 * data_len)]
+    else:
+        sub_dataset = dataset[int(0.8 * data_len):]
+
+    # Create the dataset object
+    train_or_test = DatasetObject(dataset=sub_dataset)
+
+    return train_or_test
+
+
+def create_data_loader(dataset, batch_size):
+    loader = torch.utils.data.DataLoader(dataset=dataset,
+                                         batch_size=batch_size,
+                                         shuffle=True,
+                                         pin_memory=True, num_workers=0, drop_last=True)
+    return loader
 
 
 class Model(nn.Module):
@@ -123,32 +127,40 @@ class Model(nn.Module):
         return Variable(torch.zeros(self.num_layers * self.num_directions, self.batch_size, self.hidden_size))
 
 
-""" # Model
-Here we initialize the model with its associated parameters.
-"""
-# How many GRU layers to be stacked
-num_layers = 2
-input_size = char2vec.size
-output_size = output_char2vec.size
-hidden_size = 32
-bidirectional = True
+def make(config):
+    # Make the data
+    train, test = get_data(loaded_dataset, train=True), get_data(loaded_dataset, train=False)
+    train_loader = create_data_loader(train, batch_size=config['batch_size'])
+    test_loader = create_data_loader(test, batch_size=config['batch_size'])
 
-# Model initialization
-model = Model(input_size, hidden_size, output_size, batch_size=config['batch_size'], num_layers=num_layers,
-              bidirectional=bidirectional)
+    """ # Model
+    Here we initialize the model with its associated parameters.
+    """
+    # How many GRU layers to be stacked
+    num_layers = 2
+    input_size = char2vec.size
+    output_size = output_char2vec.size
+    hidden_size = 32
+    bidirectional = True
 
-""" # Optimizer & Loss
-"""
-learning_rate = 0.001
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    # Model initialization
+    model = Model(input_size, hidden_size, output_size, batch_size=config['batch_size'], num_layers=num_layers,
+                  bidirectional=bidirectional)
 
-# Note: To deal with imbalanced scenario, I used a weighted loss function.
-# The coefficients are arbitrary and the first one is associated with "no punctuation" case.
-# Further details:
-# https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html
-# https://discuss.pytorch.org/t/passing-the-weights-to-crossentropyloss-correctly/14731/8
-weights = torch.tensor([1., 10., 10., 10., 10., 10.])
-loss_weighted = nn.CrossEntropyLoss(weight=weights)
+    """ # Optimizer & Loss
+    """
+    learning_rate = 0.001
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Note: To deal with imbalanced scenario, I used a weighted loss function.
+    # The coefficients are arbitrary and the first one is associated with "no punctuation" case.
+    # Further details:
+    # https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html
+    # https://discuss.pytorch.org/t/passing-the-weights-to-crossentropyloss-correctly/14731/8
+    weights = torch.tensor([1., 10., 10., 10., 10., 10.])
+    criterion = nn.CrossEntropyLoss(weight=weights)
+
+    return model, train_loader, test_loader, criterion, optimizer
 
 
 def prepare_input_output(sources):
@@ -173,103 +185,135 @@ def _prepare_by_pad(sents, max_len, filler):
     return padded_seq
 
 
-def process_input_foward_pass(input_, target_, hidden):
+def process_char_to_idx(input_, target_):
     # Characters to indexes
     source_shape = [len(input_), len(input_[0])]
-    char_to_idx = torch.LongTensor([[[char2vec.get_ind(char)] for char in src] for src in input_])
-    char_to_idx = char_to_idx[..., 0]
+    input_ = torch.LongTensor([[[char2vec.get_ind(char)] for char in src] for src in input_])
+    input_ = input_[..., 0]
+
+    # Get the target variables
+    target_ = Variable(output_char2vec.char_code_batch(target_))
+    # u, counts = np.unique(target_vec, return_counts=True)
+
+    # # To device
+    # input_, target_ = input_.to(device), target_.to(device)
+
+    return input_, target_
+
+
+def train(model, train_loader, test_loader, criterion, optimizer, config):
+    # Run training and track with wandb
+    total_batches = len(train_loader) * config['NUM_EPOCHS']
+    example_ct = 0  # number of examples seen
+    batch_ct = 0
+    for epoch in range(config['NUM_EPOCHS']):
+        for _, (sent_lengths, sources) in enumerate(tqdm(train_loader)):
+
+            loss = train_batch(sent_lengths, sources, model, optimizer, criterion)
+            batch_ct += 1
+
+            if batch_ct % config['BATCH_TO_SHOW_ACCURACY'] == 0:
+                print('Epoch {:d} Batch {}'.format(epoch + 1, batch_ct))
+                print("=================================")
+
+                with torch.no_grad():
+                    # Get data
+                    test_sent_lengths, test_sources = next(iter(test_loader))
+
+                    max_len_test = int(max(test_sent_lengths))
+                    # seq_len_test = data.fuzzy_chunk_len(max_len_test, seq_length)
+
+                    # Prepare and process
+                    input_srcs_test, punc_targs_test = prepare_input_output(test_sources)
+
+                    # Pad sequences to have equal length
+                    input_no_tensor = _prepare_by_pad(input_srcs_test, max_len_test, filler=[" "])
+                    target_no_tensor = _prepare_by_pad(punc_targs_test, max_len_test, filler=["<nop>"])
+
+                    # Forward pass
+                    input_, target_ = process_char_to_idx(input_no_tensor, target_no_tensor)
+
+                    # Get the embedding
+                    embeded = model.embedding(torch.LongTensor(input_))
+
+                    # Initialize hidden
+                    hidden = model.init_hidden()
+
+                    # Forward pass to the model
+                    output, hidden = model(embeded, hidden)
+
+                    # Prediction probabilities
+                    probs = F.softmax(output.view(-1, model.output_size), dim=1
+                                      ).view(config['batch_size'], -1, model.output_size)
+
+                    # Use argmax to extract predicted labels
+                    indexes = torch.argmax(probs, axis=2)
+
+                    # punctuation_output
+                    punctuation_output = output_char2vec.vec2list_batch(indexes)
+
+                    metric.print_pc(utils.flatten(punctuation_output), utils.flatten(target_no_tensor))
+                    print('\n')
+
+            if batch_ct % config['BATCH_TO_SHOW_PREDICTION'] == 0:
+                validate_target = data.apply_punc(input_no_tensor[0], target_no_tensor[0])
+                result = data.apply_punc(input_no_tensor[0],
+                                         punctuation_output[0])
+                print(validate_target)
+                print(result)
+
+
+def train_batch(sent_lengths, sources, model, optimizer, criterion):
+
+    # Get the max len of sent
+    max_len = int(max(sent_lengths))
+
+    # Process & prepare
+    input_srcs, punc_targs = prepare_input_output(sources)
+
+    # Initialize hidden
+    hidden = model.init_hidden()
+
+    input_ = _prepare_by_pad(input_srcs, max_len, filler=[" "])
+    target_ = _prepare_by_pad(punc_targs, max_len, filler=["<nop>"])
+
+    # Reset gradients
+    optimizer.zero_grad()
+
+    # Process input target
+    input_, target_ = process_char_to_idx(input_, target_)
 
     # Get the embedding
-    embeded = model.embedding(torch.LongTensor(char_to_idx))
+    embeded = model.embedding(torch.LongTensor(input_))
 
     # Forward pass to the model
     output, hidden = model(embeded, hidden)
 
-    # Get the target variables
-    target_vec = Variable(output_char2vec.char_code_batch(target_))
-    # u, counts = np.unique(target_vec, return_counts=True)
+    #### Calculate loss ####
+    # Flatten the characters along batches and sequences.
+    loss = criterion(output.view(-1, model.output_size), target_.view(-1))
 
-    return output, hidden, target_vec
+    # Backward pass ⬅
+    optimizer.zero_grad()
+    loss.backward()
+
+    # Step with optimizer
+    optimizer.step()
+
+    return loss
 
 
-for epoch in range(config['NUM_EPOCHS']):
+def model_pipeline(config):
 
-    # Create empty batch loss
-    losses = []
+    # make the model, data, and optimization problem
+    model, train_loader, test_loader, criterion, optimizer = make(config)
 
-    for batch_i, (sent_lengths, sources) in enumerate(tqdm(train_loader)):
+    # and use them to train the model
+    train(model, train_loader, test_loader, criterion, optimizer, config)
 
-        # Get the max len of sent
-        max_len = int(max(sent_lengths))
+    # # and test its final performance
+    # test(model, test_loader)
 
-        # Process & prepare
-        input_srcs, punc_targs = prepare_input_output(sources)
+    return model
 
-        # Initialize loss (batch loss)
-        loss = 0
-
-        # Initialize hidden
-        hidden = model.init_hidden()
-
-        input_ = _prepare_by_pad(input_srcs, max_len, filler=[" "])
-        target_ = _prepare_by_pad(punc_targs, max_len, filler=["<nop>"])
-
-        # Reset gradients
-        optimizer.zero_grad()
-
-        # Process input target
-        output, hidden, target_vec = process_input_foward_pass(input_, target_, hidden)
-
-        #### Calculate loss ####
-        # Flatten the characters along batches and sequences.
-        out_reshaped = output.view(-1, model.output_size)
-        new_loss = loss_weighted(output.view(-1, model.output_size), target_vec.view(-1))
-        loss += new_loss
-
-        # Backward computation for the batch
-        loss.backward()
-        optimizer.step()
-
-        # Losses indicate the batch losses stored in a list
-        losses.append(loss.cpu().data.numpy())
-
-        if (batch_i + 1) % config['BATCH_TO_SHOW_ACCURACY'] == 0:
-            print('Epoch {:d} Batch {}'.format(epoch + 1, batch_i + 1))
-            print("=================================")
-
-            with torch.no_grad():
-                # Get data
-                test_sent_lengths, test_sources = next(iter(test_loader))
-
-                max_len_test = int(max(test_sent_lengths))
-                # seq_len_test = data.fuzzy_chunk_len(max_len_test, seq_length)
-
-                # Prepare and process
-                input_srcs_test, punc_targs_test = prepare_input_output(test_sources)
-
-                # Pad sequences to have equal length
-                input_ = _prepare_by_pad(input_srcs_test, max_len_test, filler=[" "])
-                target_ = _prepare_by_pad(punc_targs_test, max_len_test, filler=["<nop>"])
-
-                # Forward pass
-                output, hidden, target_vec = process_input_foward_pass(input_, target_, hidden)
-
-                # Prediction probabilities
-                probs = F.softmax(output.view(-1, model.output_size), dim=1
-                                  ).view(config['batch_size'], -1, model.output_size)
-
-                # Use argmax to extract predicted labels
-                indexes = torch.argmax(probs, axis=2)
-
-                # punctuation_output
-                punctuation_output = output_char2vec.vec2list_batch(indexes)
-
-                metric.print_pc(utils.flatten(punctuation_output), utils.flatten(target_))
-                print('\n')
-
-        if (batch_i + 1) % config['BATCH_TO_SHOW_PREDICTION'] == 0:
-            validate_target = data.apply_punc(input_[0], target_[0])
-            result = data.apply_punc(input_[0],
-                                     punctuation_output[0])
-            print(validate_target)
-            print(result)
+model = model_pipeline(config)
